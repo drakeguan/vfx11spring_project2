4712dd6d81a8bf65552053cbc426658c38ed554e
Shuen Huei Guan <drake.guan@gmail.com>
2011-04-08 01:07:54 +0800

    Fix default value of arguments in main().

--- main.m
+++ main.m
@@ -1,12 +1,13 @@
 function main(source)
-    if ~exist(source)
+    if ~exist('source')
 	source = '/tmp/drake/denny00.jpg';
     end
 
     filename = 'featuresOverImage.png';
     image = imread(source);
 
-    [featureX, featureY] = featureMoravec(image);
+    %[featureX, featureY] = featureMoravec(image);
+    [featureX, featureY] = featureHarris(image);
     plotFeaturesOverImage(image, featureX, featureY, filename);
 
 end
