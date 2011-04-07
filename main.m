5462f60d39e0d91d2cd0f8f39ee2463b8cbc44ad
Shuen Huei Guan <drake.guan@gmail.com>
2011-04-07 23:23:23 +0800

    Add Moravec corner detector.

---
+++ main.m
@@ -0,0 +1,13 @@
+function main(source)
+
+if ~exist(source)
+    source = '/tmp/drake/denny00.jpg';
+end
+
+filename = 'featuresOverImage.png';
+image = imread(source);
+
+[featureX, featureY] = featureMoravec(image);
+plotFeaturesOverImage(image, featureX, featureY, filename);
+
+end
