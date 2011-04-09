5aac871886b1fec037ba8b31ca744edaff400962
Shuen Huei Guan <drake.guan@gmail.com>
2011-04-09 14:42:03 +0800

    Update plotting.

--- main.m
+++ main.m
@@ -3,13 +3,15 @@ function main(source)
 	source = '/tmp/drake/denny00.jpg';
     end
 
-    filename = 'featuresOverImage.png';
+    %filename = 'featuresOverImage.png';
     image = imread(source);
 
     %[featureX, featureY] = featureMoravec(image);
-    [featureX, featureY] = featureHarris(image, 5, 2);
+    [featureX, featureY, R] = featureHarris(image, 5, 2);
     numel(featureX)
     %disp(sprintf('feature #: %d', numel(featureX));
-    plotFeaturesOverImage(image, featureX, featureY, filename);
+    plotFeaturesOverImage(image, featureX, featureY, '+');
 
+    figure(1);
+    imshow(R);
 end
