3553baf4faab935b67868d0664ebbb06d6a04132
Shuen Huei Guan <drake.guan@gmail.com>
2011-04-12 22:41:54 +0800

    Remove boundary features.

--- main.m
+++ main.m
@@ -1,15 +1,17 @@
-function main(source)
+function main(source, filename)
     if ~exist('source')
 	source = '/tmp/drake/denny00.jpg';
     end
 
-    %filename = 'featuresOverImage.png';
+    if ~exist('filename')
+	filename = '/tmp/featureHarris_result.png';
+    end
     image = imread(source);
 
     %[featureX, featureY] = featureMoravec(image);
     [featureX, featureY, R] = featureHarris(image, 7, 1, 5);
     disp(sprintf('feature number: %d.', numel(featureX)));
-    plotFeaturesOverImage(image, featureX, featureY, '+');
+    plotFeaturesOverImage(image, featureX, featureY, '+', filename);
 
     %figure(1);
     %imshow(R);
