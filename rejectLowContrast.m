065977c495f869678a07980fd9a370f3291043d3
Shuen Huei Guan <drake.guan@gmail.com>
2011-04-30 17:44:10 +0800

    ...

--- rejectLowContrast.m
+++ rejectLowContrast.m
@@ -1,4 +1,7 @@
-function [featureX, featureY, R] = rejectLowContrast(im, featureX, featureY, R)
+function [featureX, featureY, R] = rejectLowContrast(im, featureX, featureY, R, threshold)
+
+    if( ~exist('threshold') )
+	threshold = 0.1;
 
     [row, col] = size(I);
     % convert the im into luminance
@@ -13,4 +16,8 @@ function [featureX, featureY, R] = rejectLowContrast(im, featureX, featureY, R)
     if( ~isa(I, 'double'))
 	I = double(I);
     end
+
+    % Weber contrast (local)
+    Ib = mean(mean(I));
+    contrast = (I - Ib) ./ Ib;
 end
