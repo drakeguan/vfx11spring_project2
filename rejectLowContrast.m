20979a3d487c25e48765af9755f0178ac7ae17e0
Shuen Huei Guan <drake.guan@gmail.com>
2011-04-30 16:30:27 +0800

    ...

--- rejectLowContrast.m
+++ rejectLowContrast.m
@@ -1,2 +1,16 @@
-function [featureX, featureY, R] = rejectLowContrast(image, featureX, featureY, R)
+function [featureX, featureY, R] = rejectLowContrast(im, featureX, featureY, R)
+
+    [row, col] = size(I);
+    % convert the im into luminance
+    dim = ndims(im);
+    if( dim == 3 )
+	I = rgb2gray(im);
+    else
+	I = im;
+    end
+
+    % convert the image to double
+    if( ~isa(I, 'double'))
+	I = double(I);
+    end
 end
