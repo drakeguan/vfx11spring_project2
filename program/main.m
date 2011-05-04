c175a20470d0cd0ef8c1ee468722bb8c06e3714c
Shuen Huei Guan <drake.guan@gmail.com>
2011-05-04 20:29:03 +0800

    Formalize to assignment's file structure.

--- program/main.m
+++ program/main.m
@@ -15,16 +15,25 @@
     %figure(1);
     %imshow(R);
 
+    tic;
     src_img_file1 = '/tmp/drake/prtn00.jpg';
     src_img_file2 = '/tmp/drake/prtn01.jpg';
     [featureX1, featureY1, pos1, orient1, desc1, im1, R1] = featureDetection(src_img_file1, debug_);
     [featureX2, featureY2, pos2, orient2, desc2, im2, R2] = featureDetection(src_img_file2, debug_);
+    disp(sprintf('[feature detection] elapsed sec: %.2f', toc()));
 
+    tic;
     match = featureMatching(desc1, desc2, pos1, pos2);
-    match = ransac(match, pos1, orient1, desc1, pos2, orient2, desc2);
+    disp(sprintf('[feature matching] elapsed sec: %.2f', toc()));
+
+    tic;
+    matchCompact = ransac(match, pos1, orient1, desc1, pos2, orient2, desc2);
+    disp(sprintf('[ransac on features] elapsed sec: %.2f', toc()));
+
     disp('matching...');
-    %disp(match);
-    displayMatchInTerminal(pos1, pos2, match);
+    displayMatchInTerminal(pos1, pos2, matchCompact);
+    %disp('matching...');
+    %displayMatchInTerminal(pos1, pos2, matchCompact);
     %display2MatchingImage(im1, im2, pos1, pos2, match);
 
 %end
